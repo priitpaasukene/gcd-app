7e96e15ec96118365441ac3c93e904c8244f1314
Priit Pääsukene <priit.paasukene@rocket-internet.de>
2016-06-27 16:39:55 +0300

    removed SALT, as it is randomly generated.

--- _config.php
+++ _config.php
@@ -1,6 +1,5 @@
 <?php
         $DEBUG=FALSE;
-        $salt="REPLACE_WITH_BETTER_SALT";
         $memcache_server = "127.0.0.1"; // Replace with correct memcache server.
         $memcache_port = 11211;
 ?>
